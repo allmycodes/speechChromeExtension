018c27c421861a8fc9c94dc23e94fef9cdbbc3a6
allmycodes <115379946+allmycodes@users.noreply.github.com>
2022-10-08 20:21:55 -0400

    Delete initial.js

--- initial.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById('hello').addEventListener("click",function() {
-        chrome.windows.create({
-            // Just use the full URL if you need to open an external page
-            url: chrome.runtime.getURL("./STT.html")
-          });
-      });
-    });
